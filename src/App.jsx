db684ffa241d575868d5f833929a1806db4ef673
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-07-10 19:10:31 +0300

    Refactor: Modularize Code
    Improves readability and maintainability of the codebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,11 @@
-function App() {
-  const mockPost = {
-    author: 'Stephen',
-    title: 'Hello world',
-    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum facilis beatae ut laboriosam, et quisquam nisi eaque rem est nesciunt culpa tempore ad expedita rerum tempora asperiores perferendis quasi officiis.',
-    publishDate: new Date(),
-  }
+import Homepage from './components/Homepage';
+import SignUpForm from './components/authentication/SignUpForm';
+import LoginForm from './components/authentication/LoginForm';
+import AdminForm from './components/authentication/AdminForm';
 
+function App() {
   return (
-    <div className='container'>
-      <nav>
-        <a href='/'>Home</a>
-        <a href='/posts'>Posts</a>
-      </nav>
-      <header>
-        <h1>Welcome to Scribbly.</h1>
-        <div>
-          <p>
-            Explore thoughts, stories and ideas from seasoned writers.
-          </p>
-        </div>
-        <button>Start reading</button>
-      </header>
-      <main>
-        <h2>Posts</h2>
-        <section className='post'>
-          <h3>{mockPost.title}</h3>
-          <p>
-            {mockPost.content.slice(0, 100)} ...
-            <a href="/">Read more</a>
-            </p>
-          <p>
-            Published on: {(mockPost.publishDate).toDateString()} by 
-            <span> {mockPost.author}</span>  
-          </p>
-        </section>
-      </main>
-      <footer>
-        <p>Scribbly 2025 by Stephen Asembo.</p>
-      </footer>
-    </div>
+    <SignUpForm />
   )
 }
 
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,36 @@
+export default function Homepage({mockPost}) {
+  return(
+    <div className='container'>
+    <nav>
+      <a href='/'>Home</a>
+      <a href='/posts'>Posts</a>
+    </nav>
+    <header>
+      <h1>Welcome to Scribbly.</h1>
+      <div>
+        <p>
+          Explore thoughts, stories and ideas from seasoned writers.
+        </p>
+      </div>
+      <button>Start reading</button>
+    </header>
+    <main>
+      <h2>Posts</h2>
+      <section className='post'>
+        <h3>{mockPost.title}</h3>
+        <p>
+          {mockPost.content.slice(0, 100)} ...
+          <a href="/">Read more</a>
+          </p>
+        <p>
+          Published on: {(mockPost.publishDate).toDateString()} by 
+          <span> {mockPost.author}</span>  
+        </p>
+      </section>
+    </main>
+    <footer>
+      <p>Scribbly 2025 by Stephen Asembo.</p>
+    </footer>
+    </div>
+  )
+}
