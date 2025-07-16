af72413c7ec7eb6ed7556fb0bfd3104ab90a9f0b
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-07-16 23:17:52 +0300

    Feat: Style The Index Page

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,20 +1,18 @@
 import { Link } from "react-router-dom"
+import styles from '../styles/Index.module.css'
 
 export default function Index() {
   return (
-    <div>
-      <h1>Scribbly</h1>
-      <p>
-        Welcome to Scribbly, a blogging app where you get to read thoughts and ideas shared.
-        Contribute to the discussion by leaving comments.
-      </p>
-      <div>
+    <div className={styles.container}>
+      <h1 className={styles.heading}>Scribbly</h1>
+      <p>Welcome to Scribbly, a blogging app where you get to read thoughts and ideas shared.</p>
+      <p>Contribute to the discussion by leaving comments.</p>
+      <div className={styles.auth}>
         <p>
           Login back to your account here. <Link to="/login">Login</Link>
         </p>
         <p>
-          Don't have an account ?
-          Create one here. <Link to="/register">Register</Link>
+          Create an account here. <Link to="/register">Register</Link>
         </p>
       </div>
     </div>
