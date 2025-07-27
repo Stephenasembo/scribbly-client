d125a74994547e97ffbcd2e1d406204b1927b630
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-07-27 14:05:49 +0300

    Feat: Make Authenticated User Details Persistent
    The current user was lost on refreshing the website

--- src/components/contextComponents/AuthComponent.jsx
+++ src/components/contextComponents/AuthComponent.jsx
@@ -1,8 +1,40 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { AuthContext } from "../context/AuthContext"
 
 export default function AuthComponent ({children}) {
   const [currentUser, setCurrentUser] = useState({})
+
+  useEffect(() => {
+    const baseUrl = import.meta.env.VITE_BASE_URL;
+    const url = `${baseUrl}auth/user`
+    async function fetchUser() {
+      let response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Authorization': token
+      }
+      });
+      if(response.status === 200) {
+      response = await response.json();
+      const user = response.user;
+      return user;
+      } else {
+        return null;
+      }
+    }
+
+    const token = localStorage.getItem('jwt');
+    if(!token) {
+      return;
+    }
+
+    fetchUser()
+      .then((data) => {
+        if(data) {
+          setCurrentUser(data);
+        }
+      })
+  }, [])
 
   return (
     <AuthContext.Provider value={{currentUser, setCurrentUser}}>
