9527026857117450c8f8233227b3ad2c9de3cc6e
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-08-08 13:36:16 +0300

    Feat: Handle Input Errors More Gracefully
    Improves User Experience

--- src/components/authentication/LoginForm.jsx
+++ src/components/authentication/LoginForm.jsx
@@ -9,6 +9,7 @@ export default function LoginForm() {
   const [keyCounter, setKeyCounter] = useState(0);
   const [formData, setFormData] = useState({});
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState({});
   const navigate = useNavigate();
   const {setCurrentUser} = useAuthContext();
   const baseUrl = import.meta.env.VITE_BASE_URL;
@@ -24,16 +25,38 @@ export default function LoginForm() {
       body: JSON.stringify(formData)
     });
     setLoading(false);
-    if (response.status !== 200) {
-      let errorData = await response.json()
-      return console.log(errorData);
+    if (response.status === 200) {
+      response = await response.json();
+      const jwt = response.token;
+      const user = response.user;
+      localStorage.setItem('jwt', `Bearer ${jwt}`);
+      setCurrentUser(user);
+      return navigate('/app');
     }
-    response = await response.json();
-    const jwt = response.token;
-    const user = response.user;
-    localStorage.setItem('jwt', `Bearer ${jwt}`);
-    setCurrentUser(user);
-    navigate('/app');
+    if (response.status === 401) {
+      let errorData = await response.json()
+      if (errorData.message === 'User not found') {
+        let errorMsg = {
+          header: 'Unathorized access.',
+          errorInput: 'Username',
+          message: 'Username not found!',
+        }
+        return setError(errorMsg)
+      }
+      if (errorData.message === 'Incorrect password') {
+        const errorMsg = {
+          header: 'Unathorized access.',
+          errorInput: 'Password',
+          message: 'Incorrect password entered!',
+        }
+        return setError(errorMsg)
+      }
+    }
+    return setError({
+      header: '500 Server Error',
+      errorInput: 'Server',
+      message: 'An internal error occured'
+    })
   }
 
   function submitForm(e) {
@@ -52,43 +75,59 @@ export default function LoginForm() {
         <div className={styles.spinner}></div>
       </div> :
       <div className={styles.container}>
-        <h1 className='heading'>Login To Your Account</h1>
-        <form action="/" method="post">
-          <p>
-            Fields marked with an asterisk(*) are required.
-          </p>
-          <label htmlFor="username">
-            Username(*):
-            <Input
-            key={`username${keyCounter}`}
-            type="text"
-            name="username"
-            id="username"
-            placeholder="John Doe"
-            isrequired={true}
-            data={formData}
-            setData={setFormData}
+        {error.errorInput === 'Server' ?
+        <div>
+          <h1>{error.header}</h1>
+          <div>
+            <p>{error.message}</p>
+          </div>
+        </div> :
+        <div>
+          <h1 className='heading'>Login To Your Account</h1>
+          <form>
+            <p>
+              Fields marked with an asterisk(*) are required.
+            </p>
+            <label htmlFor="username">
+              Username(*):
+              <Input
+              key={`username${keyCounter}`}
+              type="text"
+              name="username"
+              id="username"
+              placeholder="John Doe"
+              isrequired={true}
+              data={formData}
+              setData={setFormData}
+              />
+              {error.errorInput === 'Username' &&
+              <p className={styles.error}>{error.message}</p>
+              }
+            </label>
+            <label htmlFor="password">
+              Password(*):
+              <Input
+              key={`password${keyCounter}`}
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Enter your password"
+              isrequired={true}
+              data={formData}
+              setData={setFormData}
+              />
+              {error.errorInput === 'Password' &&
+              <p className={styles.error}>{error.message}</p>
+              }
+            </label>
+            <Button
+            className={styles.btn}
+            text='Log in'
+            onClick={submitForm}
             />
-          </label>
-          <label htmlFor="password">
-            Password(*):
-            <Input
-            key={`password${keyCounter}`}
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Enter your password"
-            isrequired={true}
-            data={formData}
-            setData={setFormData}
-            />
-          </label>
-          <Button
-          className={styles.btn}
-          text='Log in'
-          onClick={submitForm}
-          />
-        </form>
+          </form>
+        </div>
+        }
       </div>
       }
     </div>
--- src/components/authentication/SignUpForm.jsx
+++ src/components/authentication/SignUpForm.jsx
@@ -8,6 +8,7 @@ export default function SignUpForm() {
   const [keyCounter, setKeyCounter] = useState(0);
   const [formData, setFormData] = useState({});
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState({});
   const navigate = useNavigate()
   const baseUrl = import.meta.env.VITE_BASE_URL;
   const url = `${baseUrl}auth/sign-up`
@@ -26,10 +27,18 @@ export default function SignUpForm() {
       response = await response.json();
       const jwt = response.data.token;
       localStorage.setItem('jwt', `Bearer ${jwt}`)
-      navigate('/app')
-    } else {
-      console.error(response)
+      return navigate('/app')
     }
+    if(response.status === 400) {
+      response = await response.json();
+      return setError({
+        message: response.message,
+      })
+    }
+    return setError({
+      header: '505 Server Error',
+      message: 'An internal server error occured.'
+    })
   }
 
   function submitForm(e) {
@@ -48,55 +57,66 @@ export default function SignUpForm() {
         <div className={styles.spinner}></div>
       </div> :
       <div className={styles.container}>
-        <h1 className="heading">Register</h1>
-        <p>Fill out the required details to become a member of Scribbly.</p>
-        <p>Fields are marked with an asterisk(*) are required.</p>
-        <form action="/sign-up" method="post">
-          <label htmlFor="username">
-            Username(*):
-            <Input
-            key={`username${keyCounter}`}
-            type="text"
-            name="username"
-            id="username"
-            placeholder="John Doe"
-            isrequired={true}
-            data={formData}
-            setData={setFormData}
+        {error.header ?
+        <div>
+          <h1>{error.header}</h1>
+          <p>{error.message}</p>
+        </div>:
+        <div>
+          <h1 className="heading">Register</h1>
+          <p>Fill out the required details to become a member of Scribbly.</p>
+          <p>Fields are marked with an asterisk(*) are required.</p>
+          <form action="/sign-up" method="post">
+          {error.message &&
+          <p>{error.message}</p>
+          }
+            <label htmlFor="username">
+              Username(*):
+              <Input
+              key={`username${keyCounter}`}
+              type="text"
+              name="username"
+              id="username"
+              placeholder="John Doe"
+              isrequired={true}
+              data={formData}
+              setData={setFormData}
+              />
+            </label>
+            <label htmlFor="email">
+              Email(*):
+              <Input
+              key={`email${keyCounter}`}
+              type="email"
+              name="email"
+              id="email"
+              placeholder="johndoe@gmail.com"
+              isrequired={true}
+              data={formData}
+              setData={setFormData}
+              />
+            </label>
+            <label htmlFor="password">
+              Password(*):
+              <Input
+              key={`password${keyCounter}`}
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Enter your password"
+              isrequired={true}
+              data={formData}
+              setData={setFormData}
+              />
+            </label>
+            <Button
+            className={styles.btn}
+            text='Submit'
+            onClick={submitForm}
             />
-          </label>
-          <label htmlFor="email">
-            Email(*):
-            <Input
-            key={`email${keyCounter}`}
-            type="email"
-            name="email"
-            id="email"
-            placeholder="johndoe@gmail.com"
-            isrequired={true}
-            data={formData}
-            setData={setFormData}
-            />
-          </label>
-          <label htmlFor="password">
-            Password(*):
-            <Input
-            key={`password${keyCounter}`}
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Enter your password"
-            isrequired={true}
-            data={formData}
-            setData={setFormData}
-            />
-          </label>
-          <Button
-          className={styles.btn}
-          text='Submit'
-          onClick={submitForm}
-          />
-        </form>
+          </form>
+        </div>
+        }
       </div>
       }
     </div>
