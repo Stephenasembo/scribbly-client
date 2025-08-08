a4b08967baedff1a1c5546d00e03ff75b0a4ff0e
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-08-08 12:48:17 +0300

    Feat: Add Spinners For Server Response Wait
    Improves the user experience by displaying to the user that there request
    is being handled

--- src/components/authentication/LoginForm.jsx
+++ src/components/authentication/LoginForm.jsx
@@ -8,6 +8,7 @@ import { useAuthContext } from "../context/AuthContext";
 export default function LoginForm() {
   const [keyCounter, setKeyCounter] = useState(0);
   const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const {setCurrentUser} = useAuthContext();
   const baseUrl = import.meta.env.VITE_BASE_URL;
@@ -22,6 +23,7 @@ export default function LoginForm() {
       },
       body: JSON.stringify(formData)
     });
+    setLoading(false);
     if (response.status !== 200) {
       let errorData = await response.json()
       return console.log(errorData);
@@ -36,50 +38,59 @@ export default function LoginForm() {
 
   function submitForm(e) {
     e.preventDefault();
+    setLoading(true);
     setKeyCounter(keyCounter + 1);
     sendData();
     setFormData({});
   }
 
   return(
-    <div className={styles.container}>
-      <h1 className='heading'>Login To Your Account</h1>
-      <form action="/" method="post">
-        <p>
-          Fields marked with an asterisk(*) are required.
-        </p>
-        <label htmlFor="username">
-          Username(*): 
-          <Input
-          key={`username${keyCounter}`}
-          type="text"
-          name="username"
-          id="username"
-          placeholder="John Doe"
-          isrequired={true}
-          data={formData}
-          setData={setFormData}
+    <div>
+      {loading ?
+      <div className={styles.spinnerContainer}>
+        <p>Logging you in back to your account...</p>
+        <div className={styles.spinner}></div>
+      </div> :
+      <div className={styles.container}>
+        <h1 className='heading'>Login To Your Account</h1>
+        <form action="/" method="post">
+          <p>
+            Fields marked with an asterisk(*) are required.
+          </p>
+          <label htmlFor="username">
+            Username(*):
+            <Input
+            key={`username${keyCounter}`}
+            type="text"
+            name="username"
+            id="username"
+            placeholder="John Doe"
+            isrequired={true}
+            data={formData}
+            setData={setFormData}
+            />
+          </label>
+          <label htmlFor="password">
+            Password(*):
+            <Input
+            key={`password${keyCounter}`}
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Enter your password"
+            isrequired={true}
+            data={formData}
+            setData={setFormData}
+            />
+          </label>
+          <Button
+          className={styles.btn}
+          text='Log in'
+          onClick={submitForm}
           />
-        </label>
-        <label htmlFor="password">
-          Password(*): 
-          <Input
-          key={`password${keyCounter}`}
-          type="password"
-          name="password"
-          id="password"
-          placeholder="Enter your password"
-          isrequired={true}
-          data={formData}
-          setData={setFormData}
-          />
-        </label>
-        <Button
-        className={styles.btn}
-        text='Log in'
-        onClick={submitForm}
-        />
-      </form>
+        </form>
+      </div>
+      }
     </div>
   )
 }
--- src/components/authentication/SignUpForm.jsx
+++ src/components/authentication/SignUpForm.jsx
@@ -7,6 +7,7 @@ import styles from '../../styles/Form.module.css'
 export default function SignUpForm() {
   const [keyCounter, setKeyCounter] = useState(0);
   const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate()
   const baseUrl = import.meta.env.VITE_BASE_URL;
   const url = `${baseUrl}auth/sign-up`
@@ -20,6 +21,7 @@ export default function SignUpForm() {
       },
       body: JSON.stringify(formData)
     })
+    setLoading(false);
     if(response.status === 200) {
       response = await response.json();
       const jwt = response.data.token;
@@ -32,62 +34,71 @@ export default function SignUpForm() {
 
   function submitForm(e) {
     e.preventDefault();
+    setLoading(true);
     setKeyCounter(keyCounter + 1);
     sendData();
     setFormData({});
   }
 
   return (
-    <div className={styles.container}>
-      <h1 className="heading">Register</h1>
-      <p>Fill out the required details to become a member of Scribbly.</p>
-      <p>Fields are marked with an asterisk(*) are required.</p>
-      <form action="/sign-up" method="post">
-        <label htmlFor="username">
-          Username(*): 
-          <Input
-          key={`username${keyCounter}`}
-          type="text"
-          name="username"
-          id="username"
-          placeholder="John Doe"
-          isrequired={true}
-          data={formData}
-          setData={setFormData}
+    <div>
+      {loading ?
+      <div className={styles.spinnerContainer}>
+        <p>Creating your account hang tight..</p>
+        <div className={styles.spinner}></div>
+      </div> :
+      <div className={styles.container}>
+        <h1 className="heading">Register</h1>
+        <p>Fill out the required details to become a member of Scribbly.</p>
+        <p>Fields are marked with an asterisk(*) are required.</p>
+        <form action="/sign-up" method="post">
+          <label htmlFor="username">
+            Username(*):
+            <Input
+            key={`username${keyCounter}`}
+            type="text"
+            name="username"
+            id="username"
+            placeholder="John Doe"
+            isrequired={true}
+            data={formData}
+            setData={setFormData}
+            />
+          </label>
+          <label htmlFor="email">
+            Email(*):
+            <Input
+            key={`email${keyCounter}`}
+            type="email"
+            name="email"
+            id="email"
+            placeholder="johndoe@gmail.com"
+            isrequired={true}
+            data={formData}
+            setData={setFormData}
+            />
+          </label>
+          <label htmlFor="password">
+            Password(*):
+            <Input
+            key={`password${keyCounter}`}
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Enter your password"
+            isrequired={true}
+            data={formData}
+            setData={setFormData}
+            />
+          </label>
+          <Button
+          className={styles.btn}
+          text='Submit'
+          onClick={submitForm}
           />
-        </label>
-        <label htmlFor="email">
-          Email(*): 
-          <Input
-          key={`email${keyCounter}`}
-          type="email"
-          name="email"
-          id="email"
-          placeholder="johndoe@gmail.com"
-          isrequired={true}
-          data={formData}
-          setData={setFormData}
-          />
-        </label>
-        <label htmlFor="password">
-          Password(*): 
-          <Input
-          key={`password${keyCounter}`}
-          type="password"
-          name="password"
-          id="password"
-          placeholder="Enter your password"
-          isrequired={true}
-          data={formData}
-          setData={setFormData}         
-          />
-        </label>
-        <Button
-        className={styles.btn}
-        text='Submit'
-        onClick={submitForm}
-        />
-      </form>
+        </form>
+      </div>
+      }
     </div>
   )
 }
